816003540fa9a01fd8ae1e62c119266c1e2bbd7b
matsujirushi <matsujirushi@live.jp>
2017-09-29 22:54:43 +0900

    Add chat sample.

---
+++ chat/Chat.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRCore2
+{
+    public class Chat : Hub
+    {
+        public Task Send(string data)
+        {
+            return Clients.All.InvokeAsync("AddMessage", data);
+        }
+    }
+}
